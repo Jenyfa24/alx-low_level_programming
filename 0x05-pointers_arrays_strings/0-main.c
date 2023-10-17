e827de80099d81e10e684d3d1fe6bed40116d75b
Jenyfa24 <ogechukwujenyfa@gmail.com>
2023-10-17 02:55:24 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code 
  *
@@ -7,10 +8,11 @@
  */
 int main(void)
 {
-    int n;
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+int n;
+
+n = 402;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
 }
