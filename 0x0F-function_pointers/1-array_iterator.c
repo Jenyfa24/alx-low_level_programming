cb3e5e94eae52c5dcb3d3be4f73b34372acf6023
Jenyfa24 <ogechukwujenyfa@gmail.com>
2023-11-08 23:31:41 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,16 +13,14 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-   size_t i;
-
-
-   if (array && action)
-   {
-       i = 0;
-       while (i < size)
-       {
-           action(array[i]);
-           i++;
-       }
-   }
+size_t i;
+if (array && action)
+{
+i = 0;
+while (i < size)
+{
+action(array[i]);
+i++;
+}
+}
 }
